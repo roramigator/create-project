431267681ea015b9685b252a284b0d044645d7a0
roramigator <roramigator@gmail.com>
2020-04-18 02:11:37 -0400

    Update

--- templates/react/webpack.config.js
+++ templates/react/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/i,
+        test: /\.jsx?$/i,
         exclude: /node_modules/,
         loader: "babel-loader"
       },
